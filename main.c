72b46e2398bc37beb315c9520a5c7d72f9ec5dc3
gars-cmd <57665077+gars-cmd@users.noreply.github.com>
2021-11-07 15:24:37 +0000

    update syntax

--- main.c
+++ main.c
@@ -9,44 +9,44 @@ int b = 0;
 scanf("%d" , &a);
 scanf("%d" , &b);
 
-printf("The Armstrong numbers are: ");
+printf("The Armstrong numbers are:");
 for (int i = a; i <= b; i++)
 {
     if (isArmstrong(i)==1)
     {
-        printf("%d " , i);
+        printf(" %d" , i);
     }
     
 }
 printf("\n");
-printf("The Palindromes are: ");
+printf("The Palindromes are:");
 for (int i = a; i <= b; i++)
 {
     if (isPalindrome(i)==1)
     {
-        printf("%d " , i);
+        printf(" %d" , i);
     }
     
 }
 
 printf("\n");
-printf("The Prime numbers are: ");
+printf("The Prime numbers are:");
 for (int i = a; i <= b; i++)
 {
     if (isPrime(i)==1)
     {
-        printf("%d " , i);
+        printf(" %d" , i);
     }
     
 }
 
 printf("\n");
-printf("The Strong numbers are: ");
+printf("The Strong numbers are:");
 for (int i = a; i <= b; i++)
 {
     if (isStrong(i)==1)
     {
-        printf("%d " , i);
+        printf(" %d" , i);
     }
     
 }
